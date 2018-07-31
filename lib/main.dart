64473a555d67614faeaf2d914742bb61ceb29991
testisintest <34541170+testisintest@users.noreply.github.com>
2018-07-31 15:43:45 +0545

    Added Drawer by bivek

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'pages/profile.dart';
-import "pages/home/home.dart";
+import './pages/home/home.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() => runApp(new MyApp());
 
@@ -37,6 +38,55 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+      drawer: new Drawer(
+        child: new ListView(
+          children: <Widget>[
+            new UserAccountsDrawerHeader(
+              accountName: new Text("Bivek Thapa"),
+              accountEmail: new Text("beingbivek@gmail.com"),
+              currentAccountPicture: new GestureDetector(
+                onTap: () {
+                  Fluttertoast.showToast(
+        msg: "This is Your Account Picture",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIos: 1
+    );
+            },
+                child: new CircleAvatar(
+                  backgroundImage: new NetworkImage("https://bit.ly/2M41eqZ"),
+                ),
+              ),
+              decoration: new BoxDecoration(
+                image: new DecorationImage(
+                  fit: BoxFit.fill,
+                  image: new NetworkImage("https://bit.ly/2M8PmUk")
+                )
+              ),
+            ),
+            new ListTile(
+              title: new Text("Favorites"),
+              trailing: new Icon(Icons.border_right),            
+            ),
+            new ListTile(
+              title: new Text("Places"),
+              trailing: new Icon(Icons.place),  
+              onTap: () => Navigator.of(context).push(new MaterialPageRoute(builder: 
+              (BuildContext context) => new Home())),          
+            ),
+            new ListTile(
+              title: new Text("About Us"),
+              trailing: new Icon(Icons.contact_phone),            
+            ),
+            new Divider(),
+            new ListTile(
+              title: new Text("Close"),
+              trailing: new Icon(Icons.close), 
+              onTap:() => Navigator.of(context).pop(),           
+            )
+          ],
+        ),
+      ),
       appBar: new AppBar(
         title: new Text(widget.title),
         actions: <Widget>[
